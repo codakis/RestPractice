28619a05b2955efaed16a900b1acf535f5e9f60a
Akis Kontakis <akis@inked.gr>
2020-09-18 09:45:34 +0300

    adding controller and contracts

---
+++ RestPractice.API/Controllers/AuthorsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using RestPractice.API.Services;
+
+namespace RestPractice.API.Controllers
+{
+    [ApiController]
+    public class AuthorsController :ControllerBase
+    {
+        private readonly ICourseLibraryRepository _repository;
+
+        public AuthorsController(ICourseLibraryRepository repository)
+        {
+            _repository = repository ?? 
+                throw new ArgumentNullException(nameof(repository));
+        }
+        public IActionResult GetAuthors()
+        {
+            var authors = _repository.GetAuthors();
+            return new JsonResult(authors);
+        }
+    }
+}
