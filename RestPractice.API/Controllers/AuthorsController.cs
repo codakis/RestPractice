fa54c43bb7a47ffed4d185eec604fadb1c812b9e
Akis Kontakis <akis@inked.gr>
2020-09-18 10:44:50 +0300

    fixing return types and errors

--- RestPractice.API/Controllers/AuthorsController.cs
+++ RestPractice.API/Controllers/AuthorsController.cs
@@ -5,6 +5,7 @@ using RestPractice.API.Services;
 namespace RestPractice.API.Controllers
 {
     [ApiController]
+    [Route("api/authors")]
     public class AuthorsController :ControllerBase
     {
         private readonly ICourseLibraryRepository _repository;
@@ -14,10 +15,26 @@ namespace RestPractice.API.Controllers
             _repository = repository ?? 
                 throw new ArgumentNullException(nameof(repository));
         }
+       
         public IActionResult GetAuthors()
         {
             var authors = _repository.GetAuthors();
-            return new JsonResult(authors);
+            return  Ok(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {            
+            var author = _repository.GetAuthor(authorId);
+
+            if (author ==null)
+                return NotFound();
+   
+            // if (!_repository.AuthorExists(authorId))
+            // {
+            //     return NotFound();
+            // }
+            return Ok(author);
         }
     }
 }
